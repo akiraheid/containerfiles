4c2d87f50a2d1de5909ad7db29b0116721a3bcbe
Rancid Ranger <its@ya.boi>
2022-12-26 17:20:27 -0500

    add openoffice decoap

--- openoffice/build.sh
+++ openoffice/build.sh
@@ -5,13 +5,13 @@ installer=office.tar.gz
 
 version=$(bash printVersion.sh)
 
-#curl -o $installer https://versaweb.dl.sourceforge.net/project/openofficeorg.mirror/${version}/binaries/en-US/Apache_OpenOffice_${version}_Linux_x86-64_install-deb_en-US.tar.gz
+curl -o $installer https://versaweb.dl.sourceforge.net/project/openofficeorg.mirror/${version}/binaries/en-US/Apache_OpenOffice_${version}_Linux_x86-64_install-deb_en-US.tar.gz
 
 echo "Create root image..."
 podman build -t localhost/openoffice:latest -f Dockerfile
 echo "Create root image... done"
 
-#rm $installer
+rm $installer
 
 tools=(base calc draw impress math writer)
 for i in "${tools[@]}"; do
